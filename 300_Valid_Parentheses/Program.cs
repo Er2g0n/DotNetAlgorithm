5afd02e13723427c4305b5716879eea126622e89
Eragon <hnguyenngoc.h@gmail.com>
2025-08-20 11:09:07 +0700

    update explain

--- 300_Valid_Parentheses/Program.cs
+++ 300_Valid_Parentheses/Program.cs
@@ -46,4 +46,47 @@ internal class Program
         Console.WriteLine(solution.IsValid("{[]}")); // True
         Console.WriteLine(solution.IsValid("")); // True
     }
+
+
+
+    /*
+     Cấu trúc luồng xử lý:
+        foreach (char c in s) {
+        if (c là dấu mở) → push vào stack
+        else {
+            if (stack rỗng → sai) → return false
+            pop stack
+            nếu không khớp với dấu đóng → return false
+        }
+        }
+        return stack.Count == 0;
+
+     * 
+             flowchart TD
+
+        A[Start] --> B[Khởi tạo stack rỗng]
+        B --> C[Duyệt từng ký tự trong chuỗi s]
+
+        C --> D{Ký tự là dấu mở?}
+        D -- Yes --> E[Push vào stack]
+        E --> C
+
+        D -- No --> F{Stack có rỗng không?}
+        F -- Yes --> G[Return false ❌]
+
+        F -- No --> H[Pop phần tử đầu stack]
+        H --> I{Dấu đóng có khớp với dấu mở?}
+        I -- No --> G[Return false ❌]
+        I -- Yes --> C
+
+        C -->|Duyệt hết chuỗi| J{Stack có rỗng không?}
+        J -- Yes --> K[Return true ✅ Hợp lệ]
+        J -- No --> G[Return false ❌]
+
+        K --> L[End]
+        G --> L
+
+     
+     */
+
 }
