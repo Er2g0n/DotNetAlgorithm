e74d18570c7bd34a46e3f3ef6acb79b27fddb6b9
Eragon <hnguyenngoc.h@gmail.com>
2025-08-20 09:56:25 +0700

    add 2 lessons

--- Bai_05_TITV_BinarySearch/Program.cs
+++ Bai_05_TITV_BinarySearch/Program.cs
@@ -2,6 +2,53 @@
 
 internal class Program
 {
+
+
+    /*
+        " Yêu cầu tương tự như tìm kiếm nhảy
+
+        " Ý tưởng:
+        " NHỊ PHÂN = CHIA ĐÔI
+
+        " 1. Tìm giá trị phần tử nằm giữa dãy, gọi là X
+
+        " 2. Nếu X == K thì X là kết quả và thông báo chỉ số
+
+        3. Có 2 tình huống:
+        " Nếu X < K=> Tìm kiếm ở nửa sau dãy số
+
+        " Nếu X> K=> Tìm kiếm ở nữa đầu dãy số
+
+        " 4. Lặp lại các bước cho đến khi tìm thấy, hoặc đến khi khu vực tìm kiếm là rỗng (-1)
+
+    ----------------------------------------------
+                Input dãy A, K; Output: vị trí của K hoặc -1
+            Mã giả:
+            BinarySearch(A, K)
+
+            left =1, right = n
+    
+            while left <= right
+            
+    mid = (left+right)/2 // lay phan nguyen
+    
+    if a[mid] == K
+    
+    return mid
+    
+    else if a[mid]<k
+    
+    left = mid + 1
+
+            Xem sau Chia sẻ
+
+            else
+
+            right = mid - 1
+
+            return -1
+     */
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
---
+++ Bai_06_TITV_InsertionSort/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Bai_06_TITV_InsertionSort;
+
+
+
+/*
+
+
+    " Cho trước dãy số (hoặc dãy các phần tử có thể so sánh được, ví
+    dụ dãy sâu các ký tự): a1, a2, a3, ... an
+
+    "Cần sắp xếp dãy trên bằng phép toán so sánh để đạt được một
+    dãy moi có gia trị tang dần: a1 <= a2 <= a3 <= ... <= an
+
+    Các thuật toán sắp xếp theo yêu cầu trên được gọi chung là sắp
+    xếp bằng so sánh
+ 
+
+1. Thuậtt toán sắp xếp chèn (Insertion sort)
+Có 2 cách:
+Cách 1: vừa di chuyển, vừa đổi chỗ 2 phần tử
+
+InsertionSort(A,n)
+
+for i = 2 to n
+    j = i
+    while j>1 and aj <= aj-1
+    đổi chổ aj, aj-1
+    j = j - 1 
+
+Example : 5 2 6 7 3
+
+i = 1 -> n - 1
+
+i = 1
+    j = i = 1       a[j] = 2
+    w1: a[j] <= a[j] = 2
+    2 5 6 7 3
+
+i = 2
+    j = i = 2
+    2 5 6 7 3
+ 
+i = 3
+    j = i = 3       a[j] = 7
+    2 5 6 7 3
+
+i = 4 
+    j = i = 4       a[j] = 3
+    j = 4 -> 2 5 6 3 7
+
+    j = 3 -> 2 5 3 6 7
+    
+    j = 2 -> 2 3 5 6 7
+    
+    j = 1 -> 2 3 5 6 7 
+
+
+ */
+
+internal class Program
+{
+    static int insertionSort(int[] a, int n)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            int j = i;
+            while (j > 0 && a[j]<= a[j-1]) {
+                int temp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = temp;
+                j--;
+        }
+        for i = 2 
+            key = 
+    }
+    static void Main(string[] args)
+    {
+        int[] a= new int[100];
+
+        Console.WriteLine("Hello, World!");
+        insertionSort(0, 0);
+        Console.WriteLine();
+
+    }
+}
