855676969223647e784819bfb07410bc216625cf
Eragon <hnguyenngoc.h@gmail.com>
2025-08-21 16:38:45 +0700

    LeetCode Plus One

---
+++ Bai_07_TITV_SelectionSort/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Bai_07_TITV_SelectionSort;
+
+
+/*
+     Thuật toán sắp xếp chọn (Selection sort)
+    · Ý tưởng:
+    for i = 1 to n-1
+
+    Tìm phần tử nhỏ nhất trong dãy a; -> a„, đổi chỗ cho a;
+    SelectionSort(A,n)
+    for i=1 to n-1
+    i_min = i
+    v_min = ai
+    for j=i to n
+    if aj < v_min
+    v_min = aj
+    i_min = j
+    đổi chổ a¡, ai_min
+
+
+
+
+ */
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ LeetCode_66_Plus_One/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode_66_Plus_One;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
