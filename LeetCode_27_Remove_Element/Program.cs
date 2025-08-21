6f1bcc90d4890949dc175e08f345eccd6046adc9
Eragon <hnguyenngoc.h@gmail.com>
2025-08-21 10:54:11 +0700

    LeetCode Remove Element

---
+++ LeetCode_27_Remove_Element/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode_27_Remove_Element;
+
+internal class Program
+{
+
+
+    public static int RemoveElement(int[] nums, int val) 
+    {
+        
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+
+                k++;
+            }
+        }
+        return k;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter an array of integers,separated by spaces (eg: 3 2 2 3): ");
+        string input = Console.ReadLine();
+
+        string[] parts = input.Split(' ');
+
+        int[] nums = new int[parts.Length];
+
+        for (int i = 0; i < parts.Length; i++)
+        {
+            nums[i] = int.Parse(parts[i]);
+
+        }
+
+        Console.WriteLine("Input number need to remove: ");
+        int val = int.Parse(Console.ReadLine());
+
+        int k = RemoveElement(nums,val);
+
+        Console.Write("Array after remove: ");
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+
+    }
+}
