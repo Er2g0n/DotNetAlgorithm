72819a59c6d8c4323077ca9ab96a8edaf7ff5632
Eragon <hnguyenngoc.h@gmail.com>
2025-08-24 22:06:49 +0700

    LeetCode 136 Single Number;

---
+++ LeetCode_136_Single_Number/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode_136_Single_Number;
+
+internal class Program
+{
+
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                result += nums[i];
+        }
+        return result; 
+        }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
