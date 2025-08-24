5ae75e6dc452c25a50119f5492e75b58a80f5c8f
Eragon <hnguyenngoc.h@gmail.com>
2025-08-24 22:01:08 +0700

    Will do this again

---
+++ LeetCode_108_Convert_Sorted_Array_to_Binary_Search_Tree/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode_108_Convert_Sorted_Array_to_Binary_Search_Tree;
+
+// Define the TreeNode class to resolve CS0246
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+internal class Program
+{
+    public class Solution
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            return helper(nums, 0, nums.Length - 1);
+        }
+
+        private TreeNode helper(int[] nums, int start, int end)
+        {
+            if (start > end) return null;
+
+            int mid = start + (end - start) / 2;
+
+            // Simplify object initialization to resolve IDE0017
+            var root = new TreeNode
+            {
+                val = nums[mid],
+                left = helper(nums, start, mid - 1),
+                right = helper(nums, mid + 1, end)
+            };
+
+            return root;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
